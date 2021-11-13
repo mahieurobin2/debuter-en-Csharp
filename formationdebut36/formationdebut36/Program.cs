71e10193ceacc3109b2504467e90b10c207c5700
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:36:43 +0100

    Add files via upload

---
+++ formationdebut36/formationdebut36/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace formationdebut36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Projects\CSharpFundamentals\HelloWorld\HelloWorld.sln";
+
+            var dotIndex = path.IndexOf('-');
+            var extension = path.Substring(dotIndex);
+
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File name: " + Path.GetFileName(path));
+            Console.WriteLine("File name without Extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory name: " + Path.GetDirectoryName(path));
+        }
+    }
+}
