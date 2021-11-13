581cd2441fc4d64c937b5d588b4ba5bdf75a68ac
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:16:39 +0100

    Add files via upload

---
+++ formationdebut5/formationdebut5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace formationdebut5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+        }
+    }
+}
