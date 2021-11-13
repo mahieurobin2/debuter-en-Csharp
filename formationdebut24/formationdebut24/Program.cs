c37d7e317eb236155208cf4dace15bfb7306af02
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:30:11 +0100

    Add files via upload

---
+++ formationdebut24/formationdebut24/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace formationdebut24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
