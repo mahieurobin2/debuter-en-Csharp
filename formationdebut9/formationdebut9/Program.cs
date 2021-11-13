d513661d6c8c3a4eb32e29b329afda8c20a13c8b
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:19:09 +0100

    Add files via upload

---
+++ formationdebut9/formationdebut9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace formationdebut9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine((a + b) * c);
+        }
+    }
+}
