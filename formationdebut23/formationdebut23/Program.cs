96ff73850af0a88217e016820220bbff09cdeedf
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:29:33 +0100

    Add files via upload

---
+++ formationdebut23/formationdebut23/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace formationdebut23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  for (var i = 1; i <= 10; i++)
+            //  {
+            //      if (i % 2 == 0)
+            //          Console.WriteLine(i);
+            //  }
+
+            var i = 0;
+            while (i <= 10)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
