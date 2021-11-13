43ebf9859dae80b8d05307cd209b459cc015e492
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:28:26 +0100

    Add files via upload

---
+++ formationdebut21/formationdebut21/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace formationdebut21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Robin Mahieu";
+
+           // for (var i = 0; i < name.Length; i++)
+           // {
+           //    Console.WriteLine(name[i]);
+           // }
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+        }
+    }
+}
