51c42093b86f0e75a8a22ce4b8569e5b2ae7c84e
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:26:06 +0100

    Add files via upload

---
+++ formationdebut17/formationdebut17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace formationdebut17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning.");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon.");
+            } 
+            else
+            {
+                Console.WriteLine("It's evening.");
+            }    
+        }
+    }
+}
