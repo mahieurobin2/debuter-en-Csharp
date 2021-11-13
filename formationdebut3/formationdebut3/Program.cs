b9e2070deaf9fe02ebb3d76305abad11c8e052ac
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:15:46 +0100

    Add files via upload

---
+++ formationdebut3/formationdebut3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace formationdebut3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+        }
+    }
+}
