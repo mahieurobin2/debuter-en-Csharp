546a2a0a1d640f9e3118f3708c5ddb2acf77b6f6
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:33:55 +0100

    Add files via upload

---
+++ formationdebut31/StringUtility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class StringUtility
+{
+    static string SummerizeText(string text, int MaxLength = 20)
+    {
+        const int maxLength = 20;
+
+        if (text.Length < maxLength)
+            return text;
+        else
+        {
+            var words = text.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength)
+                    break;
+            }
+            var summary = String.Join(" ", summaryWords) + "...";
+            }
+        }
+    }
+}
---
+++ formationdebut31/formationdebut31/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace formationdebut31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really really really long text.";
+            var summary = StringUtility.SummerizeText(sentence);
+            Console.WriteLine(summary);
+        }
+    }
+}
