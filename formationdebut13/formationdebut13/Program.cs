1d4a0a4e452cd03d0497ec2b1d7191cb57c19ec3
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:23:51 +0100

    Add files via upload

---
+++ formationdebut13/formationdebut13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace formationdebut13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Robin";
+            var lastName = "Mahieu";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] { "Jhon", "Jack", "Mary" };
+            var formattedNames = string.Join(",", names);
+
+            var text = @"Hi Jhon
+Look into the following paths
+c:\folder1\folder2
+c:\folder3\folder4";
+            Console.WriteLine(text);
+        }
+    }
+}
