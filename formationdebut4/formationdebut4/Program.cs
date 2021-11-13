e77c703903da93fd546d8ff9452d366a7c238f92
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:16:06 +0100

    Add files via upload

---
+++ formationdebut4/formationdebut4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace formationdebut4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const float Pi = 3.14f;
+        }
+    }
+}
