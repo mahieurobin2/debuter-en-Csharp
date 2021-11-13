42a1c47134b7ccd638f208ffe938684fe66c7750
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:34:59 +0100

    Add files via upload

---
+++ formationdebut33/formationdebut33/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace formationdebut33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your name? ");
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+            Console.WriteLine("Reversed name: " + reversed);
+        }
+
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+                array[name.Length - i] = name[i - 1];
+
+            return new string(array);
+        }
+    }
+}
