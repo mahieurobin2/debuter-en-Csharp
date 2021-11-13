de5bc26cc6f2d0b00120d2670b0f4dee842579ea
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:35:39 +0100

    Add files via upload

---
+++ formationdebut34/formationdebut34/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace formationdebut34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+            File.Copy("c:\\temp\\myfile.jpg", "d:\\temp\\myfile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
+        }
+    }
+}
