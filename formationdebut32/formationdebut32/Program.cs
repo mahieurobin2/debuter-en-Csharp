a44aad59dd7c641ce032c4a64d71c70d59b9d329
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:34:28 +0100

    Add files via upload

---
+++ formationdebut32/formationdebut32/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace formationdebut32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder("Hello world");
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First Char: " + builder[0]);
+        }
+    }
+}
