3a901923e3fe23f4ae3e7ad89e9370132258d805
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:33:23 +0100

    Add files via upload

---
+++ formationdebut30/formationdebut30/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace formationdebut30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Robin Mahieu ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("FirstName: " + firstName);
+            Console.WriteLine("LastName: " + lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("FirstName: " + names[0]);
+            Console.WriteLine("LastName: " + names[1]);
+
+            Console.WriteLine(fullName.Replace("Robin", "Robi"));
+
+            if (String.IsNullOrWhiteSpace(" "))
+                Console.WriteLine("Invalid");
+
+            var str = "25";
+            var age = Convert.ToByte(str);
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            price.ToString(price.ToString("C0"));
+        }
+    }
+}
