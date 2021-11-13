bce75eaaba9ecf6495601e401be069d50001eeb3
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:17:35 +0100

    Add files via upload

---
+++ formationdebut7/formationdebut7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace formationdebut7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                 var number = "1234";
+                 byte b = Convert.ToByte(number);
+                 Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+            }
+        }
+    }
+}
