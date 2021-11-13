682c1459a02eb4c247a14b4918cb7f9c68b744a3
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:26:37 +0100

    Add files via upload

---
+++ formationdebut18/formationdebut18/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace formationdebut18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isGoldCustomer = true;
+
+            // float price;
+            // if (isGoldCustomer)
+            //     price = 19.95f;
+            //else
+            //     price = 29.95f;
+
+            float price = (isGoldCustomer) ? 19.95f : 29.95f;
+
+            Console.WriteLine(price);
+        }
+    }
+}
