7f90460d0094f9cf1a58c1695c16a44da2301ae6
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:24:18 +0100

    Add files via upload

---
+++ formationdebut14/formationdebut14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace formationdebut14
+{
+    public enum ShippingMethod
+    {
+         RegularAirMail = 1,
+         RegisteredAirMail = 2,
+         Express = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+        }
+    }
+}
