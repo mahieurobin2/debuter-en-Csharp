f8d56f3fb8c971efb8eddc0046c661ff8abacf37
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:36:10 +0100

    Add files via upload

---
+++ formationdebut35/formationdebut35/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace formationdebut35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            // var files = Directory.GetFiles(@"c:\project\CSharpFundamentals", "*.sln*", SearchOption.AllDirectories);
+            // foreach (var file in files)
+            //     Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:\projects\CSharpFundamentals", "*.*",
+                    SearchOption.AllDirectories);
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
