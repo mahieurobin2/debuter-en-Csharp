9de42fc0a5901cba2e921c61346abb5cf7c03b64
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:19:35 +0100

    Add files via upload

---
+++ formationdebut10/formationdebut10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace formationdebut10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine(!(c > b || c == a));
+        }
+    }
+}
+
