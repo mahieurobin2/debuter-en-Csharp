51d19082d8a0716c9b759f1ed4c84de90b1d2572
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:32:46 +0100

    Add files via upload

---
+++ formationdebut29/formationdebut29/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace formationdebut29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating 
+            var timeSpan = new TimeSpan(1, 2, 3);
+
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var TimeSpan2 = TimeSpan.FromHours(1);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
+
+            // Properties
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+
+            // Add
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            Console.WriteLine("Substract Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(8)));
+
+            //ToString
+            Console.WriteLine("ToString" + timeSpan.ToString());
+
+            // Parse
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
+        }
+    }
+}
