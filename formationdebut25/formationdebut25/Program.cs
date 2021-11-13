7438d90049df0fcfdf9cf421593e5ac73b0d5b50
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:30:44 +0100

    Add files via upload

---
+++ formationdebut25/formationdebut25/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace formationdebut25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength]; 
+            for (var i = 0; i < passwordLength; i++)
+                buffer[i] = (char)('a'+ random.Next(0, 26));
+
+            var password = new string(buffer);
+            Console.WriteLine();
+
+            Console.WriteLine((int)'a');  // a correspond a 97 dans la table ASCII ce qui fait que quand on va compiler la console va nous ressortir le nombre 97
+        }
+    }
+}
