dc47a1a38a44c6f90a575d30666348b1b902bea2
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-11-13 07:15:19 +0100

    Add files via upload

---
+++ formationdebut2/formationdebut2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace formationdebut2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Robin";
+            var isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}
